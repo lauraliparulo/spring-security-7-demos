acae71cc8d9466c99624301457c1524496d858b2
Laura Liparulo <laura.liparulo@capgemini.com>
2026-06-01 10:39:38 +0200

    initial commit

---
+++ src/main/java/de/jugf/spring_security_7_demos/SpringSecurity7DemosApplication.java
@@ -0,0 +1,13 @@
+package de.jugf.spring_security_7_demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity7DemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity7DemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/jugf/spring_security_7_demos/SpringSecurity7DemosApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jugf.spring_security_7_demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity7DemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
